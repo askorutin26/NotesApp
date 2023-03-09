50eedcbd464dd0dfb5fcbe095fae67a7ff0a93c0
askor26 <askorutin26@gmail.com>
2023-03-09 20:23:45 +0300

    changed routes

--- app/src/routes.js
+++ app/src/routes.js
@@ -3,12 +3,12 @@ const base = origin;
 const routes = {
   app: { home: () => "/", login: () => "/login", signUp: () => "/signup" },
   server: {
-    signUp: () => base.concat("signUp"),
-    logIn: () => base.concat("logIn"),
-    postNote: () => base.concat("note"),
-    getNotes: () => base.concat("notes"),
-    uploadImg: () => base.concat("upload"),
-    image: () => base.concat("image"),
+    signUp: () => base.concat("/signUp"),
+    logIn: () => base.concat("/logIn"),
+    postNote: () => base.concat("/note"),
+    getNotes: () => base.concat("/notes"),
+    uploadImg: () => base.concat("/upload"),
+    image: () => base.concat("/image"),
   },
 };
 
